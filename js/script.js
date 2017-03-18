a7a803bf07e04da69e3c275d988ef08bc3765ce1
Wenyu She <wenyu.she@outlook.com>
2017-03-18 11:59:57 -0700

    add detail bar

--- js/script.js
+++ js/script.js
@@ -1,49 +1,49 @@
 // *********************** show/hide sidebar *****************************
 var show = document.getElementById("map");
+var f_btn = document.getElementById("f-btn");
 
 map.addEventListener('click', function(){
-   document.getElementById('sidebar').style.width = '400px';
    document.getElementById('navbar').style.height = '0';
    document.getElementById('title').style.display = 'none';
    document.getElementById('logo').style.display = 'block';
 });
 
-map.addEventListener('dblclick', function(){
-   document.getElementById('sidebar').style.width = '0px';
+f_btn.addEventListener('click', function(){
+   document.getElementById('filterbar').style.width = '0px';
 });
 
-// *********************** End *****************************
-// *********************** flip sidebar *****************************
+// *********************** show/hide filterbar end*****************************
+// // *********************** flip filterbar *****************************
 
-Element.prototype.hasClassName = function (a) {
-    return new RegExp("(?:^|\\s+)" + a + "(?:\\s+|$)").test(this.className);
-};
+// Element.prototype.hasClassName = function (a) {
+//     return new RegExp("(?:^|\\s+)" + a + "(?:\\s+|$)").test(this.className);
+// };
 
-Element.prototype.addClassName = function (a) {
-    if (!this.hasClassName(a)) {
-        this.className = [this.className, a].join(" ");
-    }
-};
+// Element.prototype.addClassName = function (a) {
+//     if (!this.hasClassName(a)) {
+//         this.className = [this.className, a].join(" ");
+//     }
+// };
 
-Element.prototype.removeClassName = function (b) {
-    if (this.hasClassName(b)) {
-        var a = this.className;
-        this.className = a.replace(new RegExp("(?:^|\\s+)" + b + "(?:\\s+|$)", "g"), " ");
-    }
-};
+// Element.prototype.removeClassName = function (b) {
+//     if (this.hasClassName(b)) {
+//         var a = this.className;
+//         this.className = a.replace(new RegExp("(?:^|\\s+)" + b + "(?:\\s+|$)", "g"), " ");
+//     }
+// };
 
-Element.prototype.toggleClassName = function (a) {
-  this[this.hasClassName(a) ? "removeClassName" : "addClassName"](a);
-};
+// Element.prototype.toggleClassName = function (a) {
+//   this[this.hasClassName(a) ? "removeClassName" : "addClassName"](a);
+// };
 
-var init = function() {
-  var card = document.getElementById('card');
+// var init = function() {
+//   var card = document.getElementById('card');
 
-  document.getElementById('flipbtn').addEventListener( 'click', function(){
-    card.toggleClassName('flipped');
-  }, false);
-};
+//   document.getElementById('flipbtn').addEventListener( 'click', function(){
+//     card.toggleClassName('flipped');
+//   }, false);
+// };
 
-window.addEventListener('DOMContentLoaded', init, false);
+// window.addEventListener('DOMContentLoaded', init, false);
 
-// *********************** flip sidebar end *****************************
+// // *********************** flip filterbar end *****************************
